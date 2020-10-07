9a208ac1a23fde23fbe3cfd59e05dfcfe4ba8e39
Diego Eiras <eiras.lucio@gmail.com>
2020-10-06 21:34:02 -0300

    Updates

---
+++ precache-manifest.76eea09b1fbb8923a8ed21348663c454.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "360c1a25308ef5dc64b9e04ec2e811d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7c7904529bc8508efb4",
+    "url": "/static/js/2.af25f13c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.af25f13c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff2f34df76a5d3bcb74d",
+    "url": "/static/js/main.b807037e.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  },
+  {
+    "revision": "6c89e0465f5e68e340ae26675f9fab42",
+    "url": "/static/media/logo.6c89e046.svg"
+  },
+  {
+    "revision": "582c83cacb029b2dfb4f033e956147fd",
+    "url": "/static/media/phone.582c83ca.png"
+  }
+]);
