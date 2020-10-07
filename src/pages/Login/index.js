0637a5428868e9b51512efe1e3c9e6365b4543bc
Diego Eiras <eiras.lucio@gmail.com>
2020-10-07 00:48:22 -0300

    ➡ Add login screen

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,53 @@
 import React from 'react'
 
+import logo from '../../assets/logo.svg'
+import googleIcon from '../../assets/google.svg'
+import facebookIcon from '../../assets/facebook.svg'
+import notebookImg from '../../assets/notebook.png'
+
+import { 
+  Container,
+  Header,
+  Content,
+  Presentation,
+  Buttons,
+} from './styles'
+
 export default function Login() {
   return (
-    <h1>Login</h1>
+    <Container>
+      <Header>
+        <img src={logo} alt="E-Community" />
+      </Header>
+      
+      <Content>
+        <Presentation>
+          <h1>Bem vindo</h1>
+          <p>
+            Entre com a sua conta do Google ou Facebook 
+            e comece a escrever
+          </p>
+
+          <Buttons>
+            <button className="google">
+              <img src={googleIcon} alt="Google" />
+              Entrar com o Google
+            </button>
+
+            <button className="facebook">
+              <img src={facebookIcon} alt="Facebook" /> 
+              Entrar com o facebook
+            </button>
+          </Buttons>
+        </Presentation>
+
+        <img 
+          src={notebookImg} 
+          alt="Preview" 
+          height="60%"
+          width="60%"
+        />
+      </Content>
+    </Container>
   )
 }
--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -1,5 +1,88 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  flex: 1;
+  height: 100vh;
+  max-width: 1100px;
+  overflow: hidden;
+`
+
+export const Header = styled.header`
+  margin-top: 48px;
+`
+
+export const Content = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  margin-top: 64px;
+`
+
+export const Presentation = styled.div`
+  > h1 {
+    color: #3a3a3a;
+    font-size: 64px;
+    font-weight: bold;
+  }
+
+  > p {
+    max-width: 380px;
+
+    color: #737380;
+    font-size: 24px;
+    line-height: 36px;
+
+    margin-top: 24px;
+  }
+`
+
+export const Buttons = styled.div`
+  max-width: 360px;
+
+  display: flex;
+  flex-direction: column;
+
+  margin-top: 64px;
+
+  > button {
+    cursor: pointer;
+
+    height: 60px;
+    
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    border: none;
+    border-radius: 5px;
+
+    color: #fff;
+    font-size: 24px;
+    font-weight: bold;
+
+    & + button {
+      margin-top: 24px;
+    }
+
+    > img {
+      margin-right: 16px;
+    }
+  }
+
+  > .google {
+    background: #EA4335;
+    transition: background-color 0.2s;
+
+    &:hover {
+      background: #9C3128;
+    }
+  }
+
+  > .facebook {
+    background: #3B589D;
+    transition: background-color 0.2s;
+
+    &:hover {
+      background: #273965;
+    }
+  }
 `
