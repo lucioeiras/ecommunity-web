3f9fa6f58ec6b7d1c37c9e60f66ea024327e03f1
Diego Eiras <eiras.lucio@gmail.com>
2020-10-07 03:03:50 -0300

    🔥 Connect to Firebase

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
 
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
 import Routes from './routes'
 import GlobalStyles from './styles/Global'
 
 export default function App() {
+  firebase.initializeApp({
+    apiKey: "AIzaSyAoldte3KU26_gEEmX8YGme_3wukxIT-dA",
+    authDomain: "e-community-1485d.firebaseapp.com",
+    databaseURL: "https://e-community-1485d.firebaseio.com",
+    projectId: "e-community-1485d",
+    storageBucket: "e-community-1485d.appspot.com",
+    messagingSenderId: "765504482440",
+    appId: "1:765504482440:web:67ac6a3ab2a024a1a0833d",
+    measurementId: "G-C3SYPMPW9Z"
+  })
+
   return (
     <>
       <GlobalStyles />
       <Routes />
     </>
-  );
+  )
 }
